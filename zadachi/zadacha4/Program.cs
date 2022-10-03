22b50c60491a758cec5a812f5de68aaae68fd4b2
irina <irinakyleashova2519@gmail.com>
2022-10-04 00:31:24 +0300

    zadacha4

---
+++ zadachi/zadacha4/Program.cs
@@ -0,0 +1,20 @@
+﻿//принимает 3 числа и выдает max из этих чисел
+
+Console.Write("введите первое чисо: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе чисо: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье чисо: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
+
+
