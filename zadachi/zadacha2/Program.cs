c30c1a23ce0f6ec5a75959c8ec351bdb1e4f1458
irina <irinakyleashova2519@gmail.com>
2022-10-04 00:14:47 +0300

    zadacha2

---
+++ zadachi/zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿//программа, которая на вход приимает 2 числа и вдает, max  
+
+
+Console.Write("введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+int max=a|b;
+if (a>b)
+{
+    Console.WriteLine(a=max);
+}
+else
+{
+    Console.WriteLine(b=max);
+}
+
