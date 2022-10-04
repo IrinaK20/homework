66f56d1b10f7dd206d37fb6c9ac6c28587eab69b
irina <irinakyleashova2519@gmail.com>
2022-10-05 00:44:35 +0300

    zadacha8

---
+++ zadachi/zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.Write("введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+
+for(int i=2;i<=N;i+=2)
+{
+    Console.WriteLine(i);
+}
+
+
