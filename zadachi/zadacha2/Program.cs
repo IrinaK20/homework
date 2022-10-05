9666ce33d7a7dfc47742af22e03a5a7ee9ceaef0
irina <irinakyleashova2519@gmail.com>
2022-10-06 00:29:27 +0300

    корректировка задачи 2

--- zadachi/zadacha2/Program.cs
+++ zadachi/zadacha2/Program.cs
@@ -1,6 +1,6 @@
 ﻿//программа, которая на вход приимает 2 числа и вдает, max  
 
-
+/*
 Console.Write("введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine()); 
 Console.Write("введите второе число: ");
@@ -15,3 +15,23 @@ else
     Console.WriteLine(b=max);
 }
 
+*/
+
+
+Console.Write("введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+int max=a|b;
+if (a > b)
+{
+Console.WriteLine("Число "+a+" большее, а число "+b+" меньшее");
+}
+else if (b > a)
+{
+Console.WriteLine("Число "+b+" большее, а число "+a+" меньшее");
+}
+else
+{
+Console.WriteLine("Числа равны");
+}
