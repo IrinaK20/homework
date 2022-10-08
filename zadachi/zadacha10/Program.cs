e5e2b3a8f82dbe122ceb1544403be93f8c0126ca
irina <irinakyleashova2519@gmail.com>
2022-10-09 02:54:36 +0300

    дз 2-го семинара

---
+++ zadachi/zadacha10/Program.cs
@@ -0,0 +1,16 @@
+﻿// ввести трехзначное число и на выходе показать вторую цифру этого числа 
+
+Console.Write("введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100 && a<=999)
+{
+    int first = a / 10;
+    int second = first % 10;
+    Console.Write("вторая цифра: " + second);
+}
+else
+{
+    Console.Write("ввели некорректное число: ");
+}
+
---
+++ zadachi/zadacha13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*выводит третью цифру числа или сообщает, что третьей цифры нет*/
+
+Console.Write("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100)
+{
+    if (a > 999)
+    {
+        int i = 10;
+        while (a/i>1000)
+        {
+            i=i*10;
+        }
+        Console.Write("третья цифра: " +(a/i)%10);
+    }
+    else
+    {
+       Console.Write("третья цифра: " + a%10);
+    }
+
+}
+else
+{
+    Console.Write("третьей цифры нет");
+}
