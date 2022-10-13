20193a8e8ceb06e592ebfdae83237da06615a807
irina <irinakyleashova2519@gmail.com>
2022-10-14 01:48:40 +0300

    дз 3-го семинара

---
+++ zadachi/zadacha21/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит 
+расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/ 
+
+
+Console.WriteLine("A(x1;y1;z1),B(x2,y2;z2)");
+Console.Write("input x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("AB lenght is: " + Math.Round(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2- y1)+(z2-z1)*(z2-z1)), 2));
+
+
+
+// Math.Round(),2));  используется для округления 
+//Math.Sqrt это квадратный корень
---
+++ zadachi/zadacha23/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int index=0;
+int stepen=3;
+
+for (int i=1; i<=n; i++)
+{
+    index=Convert.ToInt32(Math.Pow((int) i,stepen));
+    Console.Write(index + " ");
+}
