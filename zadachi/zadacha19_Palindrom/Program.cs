eb95ceba6cb0a536048665f5717d58960aa0f2b8
irina <irinakyleashova2519@gmail.com>
2022-10-15 23:18:23 +0300

    дз 3-го семинара

--- zadachi/zadacha19_Palindrom/Program.cs
+++ zadachi/zadacha19_Palindrom/Program.cs
@@ -27,3 +27,59 @@ Console.WriteLine("Введите пятизначное число: ");
  {
      Console.WriteLine("Введено некорректное число");
  }
+
+/*
+//Другой вариант
+
+Console.WriteLine("Введите пятизначное число: "); //ВАРИАНТ №2 - Универсальный алгоритм :)
+int number = Convert.ToInt32(Console.ReadLine());
+int numberLen = (int)Math.Log10(number);
+int first = numberLen;
+int last = 1;
+int firstDigit = (int)(number / Math.Pow(10, numberLen));
+int lastDigit = (number / last) % 10;
+
+if (numberLen == 4) //Если убрать проверку этого условия, то решение будет доступно для чисел от 1 до 2,147,483,647 :)
+{
+    for (int i = 1; i <= ((numberLen + 1) / 2); i++)
+    {
+        if (firstDigit == lastDigit)
+        {
+            first = first - 1;
+            last = last * 10;
+            firstDigit = (int)(number / Math.Pow(10, first) % 10);
+            lastDigit = (number / last) % 10;
+        }
+    }
+    if (firstDigit == lastDigit)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("Значение неверно! Введите пятизначное число!");
+} 
+*/
+
+// //Третий вариант
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int numbersave = number;
+// int revnumber = 0;
+
+// while (number > 0)
+// {
+//     int dig = number % 10;
+//     revnumber = revnumber * 10 + dig;
+//     number = number / 10;
+// }
+// if (numbersave == revnumber)
+//     Console.WriteLine("Число является палиндромом");
+// else
+//     Console.WriteLine("Число не является палиндромом");
