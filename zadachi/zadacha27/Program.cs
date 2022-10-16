439c0df2165ca12f2d2f9ae9aad85f394f6bb9c8
irina <irinakyleashova2519@gmail.com>
2022-10-16 21:31:06 +0300

    дз 4-го семинара

---
+++ zadachi/zadacha27/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12   */
+
+
+Console.Write("введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int result = 0;
+
+while (a > 10)
+{
+    int a1 = a % 10;
+    sum = sum + a1;
+    a = a / 10;
+}
+if (a<10)
+{
+    result = sum + a;
+}
+
+Console.WriteLine($"Сумма цифр во введенном числе равна {result}");
---
+++ zadachi/zadacha29_zapolnit'Massiv/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Напишите программу, которая задаёт массив произвольной длины, заполняет произвольными элементами
+ и выводит их на экран. Длину массива и элементы массива можно задать рандомно или 
+ попросить пользователя ввести в консоли.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/ 
+
+
+void newArray(int[] array)
+{
+     int length = array.Length;
+     int index = 0;
+    while (index < length)
+     {
+         Console.WriteLine($"Введите элемент массива [{index}]:");
+         int N = Convert.ToInt32(Console.ReadLine());
+         array[index] = N;
+         index++;
+     }
+ }
+ void PrintArray(int[] arr)
+ {
+     int count = arr.Length;
+     int index = 0;
+     while (index < count)
+     {
+        Console.Write(arr[index] + " ");
+         index++;
+     }
+}
+ int[] array = new int[8];
+ int count = array.Length;
+
+ newArray(array);
+ PrintArray(array);
