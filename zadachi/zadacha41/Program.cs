e04b71e6b76223ba85305132301d806319aef378
irina <irinakyleashova2519@gmail.com>
2022-10-24 01:55:24 +0300

    дз 6

---
+++ zadachi/zadacha41/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int m = 5;
+int count = 0;
+
+for (int i = 0; i< m; i++)
+{
+    Console.Write($"введите {i+1} число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 0) count++;
+}
+Console.WriteLine(count + " чисел больше нуля");
---
+++ zadachi/zadacha43/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.WriteLine("значения b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("значения k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("значения b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("значения k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x;
+double y;
+
+if (k1 != k2)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+
+    Console.WriteLine($"прямые имеют точку пересечения ({x}, {y})");
+}
+else
+{
+        Console.WriteLine("прямые параллельны");
+}
